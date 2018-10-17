4f8ab36478ec7cc81ee29bb643eb9098834d18a8
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-10-17 18:43:38 +0100

    changes to the token page

--- components/constants/mybit-graphic.js
+++ components/constants/mybit-graphic.js
@@ -5,7 +5,11 @@ export const graphicInfo = {
   content:
     <Fragment>
       <p>
-      The MyBit Network is powered by the MyBit Token (MYB). It’s used for everything from executing specific functions with our SDKs to accessing features of applications. Basically, all apps built on MyBit (including MyBitGO) use MYB to function. The token’s also used to fund the growth of the network through MyBit Ventures and other developer reward programmes.
+      The MyBit Network is powered by the MyBit Token (MYB).
+      MYB is used for everything from executing specific functions with our SDKs
+      to accessing features of applications. Basically, all apps built on MyBit
+      (including MyBitGO) use MYB to function. The token is also used to fund
+      the growth of the network through MyBit Ventures and other developer reward programmes.
       </p>
     </Fragment>,
   graphic:
