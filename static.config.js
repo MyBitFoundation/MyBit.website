11b0706fde4546d20c4039bc9de091cf632acb24
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-06-19 10:52:05 -0500

    Had the parameters inverted because apparently I ate glue when I was a kid

--- static.config.js
+++ static.config.js
@@ -32,11 +32,11 @@ export default {
       path: 'tracker',
       component: 'src/pages/tracker',
       getData: async () => {
-        const url = `https://w06a9ojvmg.execute-api.us-east-1.amazonaws.com/prod/`
+        const url = `https://y63yt6hph4.execute-api.us-east-1.amazonaws.com/prod/`
         const response = await axios.get(url)
         const chartData = response.data
         const responseB = await axios.get(
-          `https://y63yt6hph4.execute-api.us-east-1.amazonaws.com/prod?skip=0&take=10`
+          `https://w06a9ojvmg.execute-api.us-east-1.amazonaws.com/prod?skip=0&take=10`
         )
         const tableData = responseB.data
         return { chartData, tableData }
